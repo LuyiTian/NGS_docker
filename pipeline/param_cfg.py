070ae45d0f6d853fce3a8a7bd7ab81ddb70b9115
Luyi Tian <tlytiger@hotmail.com>
2016-01-11 21:10:34 +0800

    -m

--- pipeline/param_cfg.py
+++ pipeline/param_cfg.py
@@ -21,6 +21,6 @@ file_cfg = {
 }
 
 bwa_mem_cfg = {
-    "-R": r"@RG\tID:group1\tSM:sample1\tLB:lib1\tPL:illumina\tPU:unit1",
+    "-R": r"'@RG\tID:group1\tSM:sample1\tLB:lib1\tPL:illumina\tPU:unit1'",
     "-M": ""
 }
